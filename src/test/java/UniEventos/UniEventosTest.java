46f130bcccd3f126ce24edffa81b17b461b5e104
Catacc0421 <catalina.clavijoc@uqvirtual.edu.co>
2024-05-23 11:44:45 -0500

    se cambió el test

--- src/test/java/UniEventos/UniEventosTest.java
+++ src/test/java/UniEventos/UniEventosTest.java
@@ -1,26 +1,17 @@
 package UniEventos;
 
 
-import co.edu.uniquindio.uniEventos.modelo.Evento;
-import co.edu.uniquindio.uniEventos.modelo.Localidad;
-import co.edu.uniquindio.uniEventos.modelo.UniEventos;
-import co.edu.uniquindio.uniEventos.modelo.Usuario;
+import co.edu.uniquindio.uniEventos.modelo.*;
 import co.edu.uniquindio.uniEventos.modelo.enums.TipoEvento;
-import co.edu.uniquindio.uniEventos.utils.EnvioEmail;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
-import static co.edu.uniquindio.uniEventos.modelo.UniEventos.miUniEventos;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UniEventosTest {
-
-
     private UniEventos uniEventos;
 
     @BeforeEach
@@ -59,16 +50,6 @@ public class UniEventosTest {
         assertEquals(email, usuario.getEmail());
     }
 
-    @Test
-    public void testBuscarEventoCodigo() throws Exception {
-        ArrayList<Localidad> localidades = new ArrayList<>();
-        localidades.add(new Localidad("VIP", 100, 200, 0));
-        uniEventos.crearEvento("Test Event 4", "City", "Description", TipoEvento.CONCIERTO, "image4.jpg", LocalDate.now().plusDays(30), "Address4", localidades);
-        Evento evento = uniEventos.listarEventos().get(0);
-        Evento eventoBuscado = uniEventos.buscarEventoCodigo(evento.getCodigo());
-        assertNotNull(eventoBuscado);
-        assertEquals(evento.getCodigo(), eventoBuscado.getCodigo());
-    }
 
     @Test
     public void testCrearLocalidad() throws Exception {
@@ -91,26 +72,13 @@ public class UniEventosTest {
         });
     }
 
-    @Test
-    public void testCrearEvento() {
-        assertDoesNotThrow(() -> {
-            ArrayList<Localidad> localidades = new ArrayList<>();
-            localidades.add(new Localidad("VIP", 100, 200, 0));
-            boolean result = uniEventos.crearEvento("Concierto de Rock", "Bogotá", "El mejor concierto de rock del año", TipoEvento.CONCIERTO, "imagen.jpg", LocalDate.of(2024, 6, 20), "Dirección del evento", localidades);
-            assertTrue(result);
-        });
+
     }
 
-    @Test
-    public void testModificarEvento() {
-        assertDoesNotThrow(() -> {
-            ArrayList<Localidad> localidades = new ArrayList<>();
-            localidades.add(new Localidad("VIP", 100, 200, 0));
-            uniEventos.crearEvento("Concierto de Rock", "Bogotá", "El mejor concierto de rock del año", TipoEvento.CONCIERTO, "imagen.jpg", LocalDate.of(2024, 6, 20), "Dirección del evento", localidades);
 
-            Evento evento = uniEventos.listarEventos().get(0);
-            boolean result = uniEventos.modificarEvento(evento.getCodigo(), "Concierto de Pop", "Bogotá", "El mejor concierto de pop del año", TipoEvento.CONCIERTO, "imagen.jpg", LocalDate.of(2024, 6, 25), "Nueva dirección del evento");
-            assertTrue(result);
-        });
-    }
-}
+
+
+
+
+
+
